eedb3e796a9539f7cce2592ab83c65338dcacfff
golubev.dmitriy <golubev.dmitriy@ITM90000916>
2017-08-02 17:34:07 +0500

    Подготовил площадку для работы с тестами.

---
+++ Task 1.Tests/DomainModel/Repository/DBRepositoryTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DomainModel.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel.Repository.Tests
+{
+    [TestClass()]
+    public class DBRepositoryTests
+    {
+        [TestMethod()]
+        public void DBRepository()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Create()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Delete()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Edit()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Get()
+        {
+            Assert.Fail();
+        }
+    }
+}
