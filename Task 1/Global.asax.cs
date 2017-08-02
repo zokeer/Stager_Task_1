cc31fa015cf1b91535361deb3cf96ef24d29768d
golubev.dmitriy <golubev.dmitriy@ITM90000916>
2017-08-02 17:32:05 +0500

    Убрал ненужное.

--- Task 1/Global.asax.cs
+++ Task 1/Global.asax.cs
@@ -17,14 +17,7 @@ namespace Task_1
         protected void Application_Start()
         {
             var builder = new ContainerBuilder();
-
-            //var file_repository = ConfigurationManager.AppSettings["FileRepositoryPath"];
-            //var full_path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, file_repository);
-            //builder.RegisterInstance(new FileRepository(full_path))
-            //    .AsSelf()
-            //    .AsImplementedInterfaces()
-            //    .SingleInstance();
-
+            
             var connection_string = ConfigurationManager.ConnectionStrings["DefaultConnection"]
                 .ConnectionString;
             builder.RegisterInstance(new DBRepository(connection_string))
