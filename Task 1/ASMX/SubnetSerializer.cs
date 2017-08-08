09ea41298e4f36b290118cc0898da52e790cb2bc
golubev.dmitriy <golubev.dmitriy@ITM90000916>
2017-08-08 15:14:48 +0500

    Напихал комментов

--- Task 1/ASMX/SubnetSerializer.cs
+++ Task 1/ASMX/SubnetSerializer.cs
@@ -4,8 +4,21 @@ using DomainModel.Models;
 
 namespace Task_1.ASMX
 {
+    /// <summary>
+    /// Класс предоставляет методы для сериализации Subnet в различном виде:
+    /// экземпляр класса Subnet, список экземпляров класса Subnet, словарь, устроенный по принципу
+    /// ключ - Subnet, значение - список Subnet.
+    /// </summary>
     public static class SubnetSerializer
     {
+        /// <summary>
+        /// Сериализует экземпляр класса Subnet.
+        /// </summary>
+        /// <param name="subnet">Экземпляр класса Subnet</param>
+        /// <returns>
+        /// Сериализованное представление переданного экземпляра
+        /// в виде нового экземпляра класса SerializableSubnet
+        /// </returns>
         public static SeriablizableSubnet SerializeSubnet(Subnet subnet)
         {
             return new SeriablizableSubnet
@@ -14,9 +27,16 @@ namespace Task_1.ASMX
                 Address = subnet.Network.Network.ToString(),
                 Mask = subnet.Network.Cidr.ToString()
             };
-
         }
 
+        /// <summary>
+        /// Сериализует список экземпляров класса Subnet.
+        /// </summary>
+        /// <param name="subnets">Список экземпляров класса Subnet.</param>
+        /// <returns>
+        /// Сериализованное представление списка в виде 
+        /// массива новых экземпляров класса SerializableSubnet.
+        /// </returns>
         public static SeriablizableSubnet[] SerializeList(List<Subnet> subnets)
         {
             return subnets
@@ -24,6 +44,14 @@ namespace Task_1.ASMX
                 .ToArray();
         }
 
+        /// <summary>
+        /// Сериализует словарь вида: ключ - Subnet, значение - Список Subnet.
+        /// </summary>
+        /// <param name="dictionary">Словарь вида: ключ - Subnet, значение - Список Subnet.</param>
+        /// <returns>
+        /// Сериализованное представление словаря в виде нового словаря, где
+        /// ключ - SerializableSubnet, значение - массив SerializableSubnet.
+        /// </returns>
         public static Dictionary<SeriablizableSubnet, SeriablizableSubnet[]> SerializeDictionary(
             Dictionary<Subnet, List<Subnet>> dictionary)
         {
